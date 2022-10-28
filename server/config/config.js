4051b25b4afd746bd218d9fa45c42346315328db
Riccardo Reali <rreali@my.centennialcollege.ca>
2022-10-27 20:48:33 -0400

    working

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,2 @@
-export const MongoURI = "mongodb://localhost:27017/media";
+export const MongoURI = "mongodb+srv://riccardo91:riccardo123@cluster0.fhc4vha.mongodb.net/books?retryWrites=true&w=majority"// ATLAS CONNECTION STRING";
 export const Secret = "someSecret";
